4ec619e1f3295481d23b5d7c359407244b167330
Errol Lin <errollin@qq.com>
2023-01-19 19:19:35 +0800

    Update the import path of the item type structure

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,4 @@
 import type { Expense } from 'models/interfaces/expense.interface';
-import type { Item } from './interfaces/item.interface';
+import type { Item } from 'models/interfaces/item.interface';
 
 export type { Expense, Item };
